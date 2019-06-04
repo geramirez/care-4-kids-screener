fba0dab62ae133fcbe65224b1464f3416a613667
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2019-06-04 16:14:54 -0500

    Creating basic screener

---
+++ screener.js
@@ -0,0 +1,18 @@
+ class Screener {
+    constructor(ui) {
+        this.ui = ui
+    }
+    review(applicant) {
+        if (this.meetsRequirements(applicant))
+            this.ui.eligible();
+        else
+            this.ui.notEligible()
+    }
+    meetsRequirements(applicant) {
+        return applicant.meets_family_income_requirements &&
+            applicant.meets_working_requirements &&
+            applicant.lives_in_ct
+    }
+}
+
+module.exports = {Screener}
